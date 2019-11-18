34c794f5af672e04c94f228a0bae735f729c7985
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-18 20:17:15 +0000

    Add css style file imports to src/client/index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,15 @@
+// js
+
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-console.log(checkForName);
+// styles
+import "./styles/resets.css"
+import "./styles/base.css"
+import "./styles/header.css"
+import "./styles/form.css"
+import "./styles/footer.css"
 
+// tests
 alert("I EXIST")
 console.log("CHANGE!!");
