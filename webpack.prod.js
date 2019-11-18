67eb74790fa1b7e32b028ea80f8aaf3afc349aec
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-18 23:23:30 +0000

    Add css and sass processing to webpack.prod.js config

--- webpack.prod.js
+++ webpack.prod.js
@@ -8,10 +8,21 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.(s*)css$/,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader'
+                ],
+            },
         ]
     },
     plugins: [
