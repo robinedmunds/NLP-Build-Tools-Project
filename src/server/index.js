f5eb22f6b83464ec80b79d63e574e2d7cb470845
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-27 19:15:41 +0000

    Use path.join for / path in server/index.js. General tidy

--- src/server/index.js
+++ src/server/index.js
@@ -1,29 +1,26 @@
-var path = require("path")
-const express = require("express")
-const mockAPIResponse = require("./mockAPI.js")
-const dotenv = require("dotenv")
+var path = require("path");
+const express = require("express");
+const mockAPIResponse = require("./mockAPI.js");
+const dotenv = require("dotenv");
 dotenv.config();
-const AYLIENTextAPI = require("aylien_textapi")
+const AYLIENTextAPI = require("aylien_textapi");
 
-const app = express()
+const app = express();
+app.use(express.static("dist"));
+const LISTEN_PORT = 8080;
 
-app.use(express.static("dist"))
-
-console.log(__dirname)
+app.listen(LISTEN_PORT, function () {
+    console.log("Example app listening on port 8080!");
+});
 
 app.get("/", function (req, res) {
-    res.sendFile("dist/index.html")
-})
-
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log("Example app listening on port 8080!")
-})
+    res.sendFile(path.join(__dirname, "../../dist/index.html"));
+});
 
 app.get("/test", function (req, res) {
-    res.send(mockAPIResponse)
-})
+    res.send(mockAPIResponse);
+});
 
 app.get("/api/analyse", function (req, res) {
     res.send("hello");
-})
+});
