72e6311a375f69307054c59c06a6d6a83f8df246
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-28 19:49:53 +0000

    Tidy code in classifyURL

--- src/client/js/classifyURL.js
+++ src/client/js/classifyURL.js
@@ -2,27 +2,29 @@
 
 function classifyURL() {
   
-  function validateURL(url) {
+  function validURL(url) {
     const REGEX_URL = /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/;
-    if (REGEX_URL.test(url)) {
-      return true;
-    } else {
-      return false;
-    };
+    return REGEX_URL.test(url);
   };
-  
+
   const classifyURLHandler = (event) => {
-    console.log(event);
-    event.preventDefault();
+    const formURL = document.getElementById("url").value;
+    if (validURL(formURL)) {
+      // do stuff
+      console.log(formURL)
+    }
   };
   
   function addClassifyURLEvent() {
     const form = document.getElementById("classify-form");
-    form.addEventListener("submit", classifyURLHandler);
+    form.addEventListener("submit", (event) => {
+      event.preventDefault();
+      classifyURLHandler(event);
+    });
   };
   
   addClassifyURLEvent();
   
-}
+};
 
 export { classifyURL }
