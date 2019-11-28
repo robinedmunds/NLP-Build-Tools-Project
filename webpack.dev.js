8b3cbd15c44fef2ab6df7f3923c90f6112abac00
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-28 23:32:47 +0000

    Install regenerator-runtime module to fix async func issues

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,7 +4,10 @@ const HtmlWebPackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/client/index.js',
+    entry: [
+        'regenerator-runtime/runtime',
+        './src/client/index.js'
+    ],
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
