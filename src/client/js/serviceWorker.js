1c98095e7113cb54d53d38c011dfaf2365ad8993
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-29 22:47:02 +0000

    Prevent sw cache from caching POST request responses

--- src/client/js/serviceWorker.js
+++ src/client/js/serviceWorker.js
@@ -8,25 +8,27 @@ self.addEventListener('install', (event) => {
 });
 
 self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).then(function (response) {
-    // caches.match() always resolves
-    // but in case of success response will have value
-    if (response !== undefined) {
-      return response;
-    } else {
-      return fetch(event.request).then(function (response) {
-        // response may be used only once
-        // we need to save clone to put one copy in cache
-        // and serve second one
-        let responseClone = response.clone();
-
-        caches.open('v1').then(function (cache) {
-          cache.put(event.request, responseClone);
-        });
+  if (event.request.method !== "POST") {
+    event.respondWith(caches.match(event.request).then(function (response) {
+      // caches.match() always resolves
+      // but in case of success response will have value
+      if (response !== undefined) {
         return response;
-      }).catch(function () {
-        console.log("File not found in service-worker cache...");
-      });
-    }
-  }));
+      } else {
+        return fetch(event.request).then(function (response) {
+          // response may be used only once
+          // we need to save clone to put one copy in cache
+          // and serve second one
+          let responseClone = response.clone();
+  
+          caches.open('v1').then(function (cache) {
+            cache.put(event.request, responseClone);
+          });
+          return response;
+        }).catch(function () {
+          console.log("File not found in service-worker cache...");
+        });
+      }
+    }));
+  };
 });
