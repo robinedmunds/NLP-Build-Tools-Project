a9511a5010c7b76521890951e6d452917bf7c176
Robin Edmunds <r.edmunds@live.co.uk>
2019-11-30 00:06:07 +0000

    Add jest test, tests/classifyURL.test.js

---
+++ tests/classifyURL.test.js
@@ -0,0 +1,35 @@
+import { validURL } from "../src/client/js/classifyURL"
+
+describe("Test classifyURL functions", () => {
+  test("validURL(url) should return FALSE when an invalid url string is passed.", () => {
+    const input = [
+      "",
+      ".",
+      "http://",
+      "https://",
+      "www",
+      "www.",
+      "abc",
+      "a",
+      "1"
+    ];
+    const output = false;
+    for (let i of input) {
+      expect(validURL(i)).toBe(output);
+    };
+  });
+
+  test("validURL(url) should return TRUE when a valid url string is passed.", () => {
+    const input = [
+      "http://google.com/",
+      "http://foo.co.uk",
+      "https://boogle.me",
+      "www.whythehate.com/",
+    ];
+    const output = true;
+    for (let i of input) {
+      expect(validURL(i)).toBe(output);
+    };
+  });
+
+});
